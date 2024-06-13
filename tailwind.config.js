63c716a20e5856ddd48be8c44b5d358205eff007
LN-Dev_007 <louisniang01@gmail.com>
2024-06-13 22:56:04 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        inter: ["Inter"],
+        petitFormat: ["Petit Format Script"],
+        russonOne: ["Russon One"],
+      }
+    },
+  },
+  plugins: [],
+});
